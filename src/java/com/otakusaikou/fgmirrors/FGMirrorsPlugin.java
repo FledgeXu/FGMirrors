45df6a7519ce97308570bacaa18adab282f91cb4
FledgeXu <xzk0701@gmail.com>
2020-03-06 18:57:49 +0800

    On branch FG3.0 Changes to be committed:       modified:   build.gradle       new file:   src/java/com/otakusaikou/fgmirrors/FGMirrorsPlugin.java       new file:   src/resources/META-INF/gradle-plugins/com.otakusaikou.fgmirrors.properties Message:       Add the FG's wrapper.

---
+++ src/java/com/otakusaikou/fgmirrors/FGMirrorsPlugin.java
@@ -0,0 +1,13 @@
+package com.otakusaikou.fgmirrors;
+
+import net.minecraftforge.gradle.userdev.UserDevPlugin;
+import org.gradle.api.Project;
+
+import javax.annotation.Nonnull;
+
+public class FGMirrorsPlugin extends UserDevPlugin {
+    @Override
+    public void apply(@Nonnull Project project) {
+        super.apply(project);
+    }
+}
